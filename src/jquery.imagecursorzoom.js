2ec2856bc1fb9f9843ddef821710933ffe4d6cdc
Bum-Seok Hwang <bitofsky@naver.com>
2015-09-10 16:23:16 +0900

    Update jquery.imagecursorzoom.js

--- src/jquery.imagecursorzoom.js
+++ src/jquery.imagecursorzoom.js
@@ -11,95 +11,92 @@
 
 "use strict";
 
-(function( window ){
+(function( window, $ ){
+
+  $.imageCursorZoom = imageCursorZoom;
+
+  $.fn.imageCursorZoom = function(){
+    return this.on('click.imageCursorZoom', function(){
+      imageCursorZoom(this);
+    });
+  };
+
+  return imageCursorZoom;
 
   /**
-   * AMD(Asynchronous Module Definition)
-   * @param {jQuery} $
+   * @param {ImgElement} elm
+   * @param {Object} option
    */
-  define(['jquery'], function( $ ){
+  function imageCursorZoom( elm, option ){
 
-    $.imageCursorZoom = imageCursorZoom;
+    var opt = $.extend(true, {/*transition: 'transform 0.05s ease-out'*/}, option),
+        $window = $(window).on('resize', onResize),
+        $body = $('BODY'),
+        $wrapper = $('<div/>'),
+        $clone = $(elm).clone().css({width : 'auto', height : 'auto', transition : opt.transition}),
+        windowWidth = 0,
+        windowHeight = 0;
 
-    $.fn.imageCursorZoom = function(){
-      return this.on('click.imageCursorZoom', function(){
-        imageCursorZoom(this);
-      });
-    };
+    $wrapper.css({
+      position : 'absolute',
+      left : 0,
+      top : 0,
+      background : 'white',
+      width : '100%',
+      height : '100%',
+      'z-index' : 9999
+    }).on('click', destroy).on('mousemove', onMousemove),
+        $wrapper.append($clone).appendTo($body);
 
-    return imageCursorZoom;
+    onResize();
 
     /**
-     * @param {ImgElement} elm
-     * @param {Object} option
+     * Tracking mouse pointer
+     * @param {Event} event
      */
-    function imageCursorZoom( elm, option ){
+    function onMousemove( event ){
 
-      var opt = $.extend(true, {/*transition: 'transform 0.05s ease-out'*/}, option),
-          $window = $(window).on('resize', onResize),
-          $body = $('BODY'),
-          $wrapper = $('<div/>'),
-          $clone = $(elm).clone().css({width : 'auto', height : 'auto', transition : opt.transition}),
-          windowWidth = 0,
-          windowHeight = 0;
+      var width = +$clone[0].clientWidth || $clone.width(),
+          height = +$clone[0].clientHeight || $clone.height(),
+          ratioX = event.pageX / windowWidth,
+          ratioY = event.pageY / windowHeight,
+          posX = -(width - windowWidth) * ratioX,
+          posY = -(height - windowHeight) * ratioY;
 
-      $wrapper.css({
-        position : 'absolute',
-        left : 0,
-        top : 0,
-        background : 'white',
-        width : '100%',
-        height : '100%',
-        'z-index' : 9999
-      }).on('click', destroy).on('mousemove', onMousemove),
-          $wrapper.append($clone).appendTo($body);
+      if( posX > 0 )
+        posX = 0;
 
-      onResize();
+      if( posY > 0 )
+        posY = 0;
 
-      /**
-       * Tracking mouse pointer
-       * @param {Event} event
-       */
-      function onMousemove( event ){
-
-        var width = +$clone[0].clientWidth || $clone.width(),
-            height = +$clone[0].clientHeight || $clone.height(),
-            ratioX = event.pageX / windowWidth,
-            ratioY = event.pageY / windowHeight,
-            posX = -(width - windowWidth) * ratioX,
-            posY = -(height - windowHeight) * ratioY;
-
-        if( posX > 0 )
-          posX = 0;
-
-        if( posY > 0 )
-          posY = 0;
-
-        $clone.css('transform', 'translate(' + posX + 'px, ' + posY + 'px)');
-
-      }
-
-      /**
-       * Get window width / height
-       * @param {Event} event
-       */
-      function onResize( event ){
-
-        windowWidth = +$window.width();
-        windowHeight = +$window.height();
-
-      }
-
-      /**
-       * Destroy all elements
-       */
-      function destroy(){
-        $clone.remove();
-        $wrapper.remove();
-      }
+      $clone.css('transform', 'translate(' + posX + 'px, ' + posY + 'px)');
 
     }
 
-  });
+    /**
+     * Get window width / height
+     * @param {Event} event
+     */
+    function onResize( event ){
 
-})(this);
+      windowWidth = +$window.width();
+      windowHeight = +$window.height();
+
+    }
+
+    /**
+     * Destroy all elements
+     */
+    function destroy(){
+      $clone.remove();
+      $wrapper.remove();
+    }
+
+  }
+
+  /**
+   * AMD(Asynchronous Module Definition)
+   */
+  define([], function(){ return imageCursorZoom; });
+
+})(this, this.jQuery);
